06700ab1b590a30b0e68e96b52f793b93ed764c1
grobocop <chasnik97876@gmail.com>
2019-07-21 14:27:48 +0200

    Drag and drop

--- SoundAnalyzer/Analyzer/analyze.py
+++ SoundAnalyzer/Analyzer/analyze.py
@@ -2,12 +2,24 @@ from scipy.io import wavfile
 from scipy import signal
 import numpy as np
 
+
 def get_sample_rate(audio_file):
     rate, data = wavfile.read(audio_file)
     return rate
 
+
 def get_first_channel(audio_file):
+    downsampling = 10
     rate, data = wavfile.read(audio_file)
-    print(data.shape)
-    data = signal.decimate(data[:,0],10)
-    return data.tolist()
+
+    time = calculate_time(len(data), rate)
+    data = signal.decimate(data[:, 0], downsampling)
+    time = signal.decimate(time[:], downsampling)
+    return data.tolist(), time.tolist()
+
+
+def calculate_time(number_of_samples, sample_rate):
+    time = np.arange(0, number_of_samples)
+    dT = 1.0 / sample_rate
+    time = np.dot(time, dT)
+    return time
--- SoundAnalyzer/Analyzer/views.py
+++ SoundAnalyzer/Analyzer/views.py
@@ -3,20 +3,16 @@ from . import analyze
 import json
 import random
 
+
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and 'audioFile' in request.FILES:
         return analyze_file(request)
     return render(request, 'Analyzer/index.html')
 
 
 def analyze_file(request):
-    audio_file = request.FILES.get("audioFile")
-    sample_rate = analyze.get_sample_rate(audio_file.file.name)
-    data = analyze.get_first_channel(audio_file)
-    x_values = [x for x in range(len(data))]
-    y_values = data #[random.randint(0,100) for x in range(100)]
-
-    return render(request, 'Analyzer/analyze.html', {'sample_rate': sample_rate,
-                                                     'x_values': json.dumps(x_values),
-                                                     'y_values': json.dumps(y_values), })
+    audio_file = request.FILES.get('audioFile')
+    data, time = analyze.get_first_channel(audio_file)
+    return render(request, 'Analyzer/analyze.html', {'x_values': json.dumps(time),
+                                                     'y_values': json.dumps(data), })
