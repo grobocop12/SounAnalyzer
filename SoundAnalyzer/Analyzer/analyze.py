c3bd418051dc0da69b7e6de67e70444cc71fc0aa
grobocop12 <chasnik97876@gmail.com>
2019-07-21 11:55:07 +0200

    style

--- SoundAnalyzer/Analyzer/analyze.py
+++ SoundAnalyzer/Analyzer/analyze.py
@@ -1,6 +1,13 @@
 from scipy.io import wavfile
-
+from scipy import signal
+import numpy as np
 
 def get_sample_rate(audio_file):
     rate, data = wavfile.read(audio_file)
     return rate
+
+def get_first_channel(audio_file):
+    rate, data = wavfile.read(audio_file)
+    print(data.shape)
+    data = signal.decimate(data[:,0],10)
+    return data.tolist()
--- SoundAnalyzer/Analyzer/views.py
+++ SoundAnalyzer/Analyzer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . import analyze
-
+import json
+import random
 
 # Create your views here.
 def index(request):
@@ -12,4 +13,10 @@ def index(request):
 def analyze_file(request):
     audio_file = request.FILES.get("audioFile")
     sample_rate = analyze.get_sample_rate(audio_file.file.name)
-    return render(request, 'Analyzer/analyze.html', {'sample_rate': sample_rate})
+    data = analyze.get_first_channel(audio_file)
+    x_values = [x for x in range(len(data))]
+    y_values = data #[random.randint(0,100) for x in range(100)]
+
+    return render(request, 'Analyzer/analyze.html', {'sample_rate': sample_rate,
+                                                     'x_values': json.dumps(x_values),
+                                                     'y_values': json.dumps(y_values), })
